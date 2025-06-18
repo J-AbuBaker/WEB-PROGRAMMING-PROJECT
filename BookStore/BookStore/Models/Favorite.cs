36db01f4f3c807af30fe50b5b4ff5e05b102ac12
Jameel Abu-Baker <100359408+J-AbuBaker@users.noreply.github.com>
2025-06-18 12:28:55 +0300

    Update Favorite.cs

--- BookStore/BookStore/Models/Favorite.cs
+++ BookStore/BookStore/Models/Favorite.cs
@@ -4,10 +4,10 @@ namespace BookStore.Models
 {
     public class Favorite
     {
-            public int UserID { get; set; }
-          public User User { get; set; }
+        public int UserID { get; set; }
+        public User User { get; set; }
+
         public int BookID { get; set; }
-      public Book Book { get; set; }
+        public Book Book { get; set; }
     }
 }
-
