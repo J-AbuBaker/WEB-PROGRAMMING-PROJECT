cb752bd95608d324f84fc4f990c2298b576dff32
3liJamal <alialjamal456@gmail.com>
2025-06-18 12:12:25 +0300

    Add required Price property with value range validation to Book model

--- BookStore/BookStore/Models/Book.cs
+++ BookStore/BookStore/Models/Book.cs
@@ -10,7 +10,9 @@ namespace BookStore.Models
      public int BookID { get; set; }   
       [Required, StringLength(100)]
         public string Title { get; set; }
-  
+          [Required, Range(0.01, 1000)]
+        public decimal Price { get; set; }
+
     }
 }
 
