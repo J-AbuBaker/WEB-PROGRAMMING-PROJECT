6e5be1c0b9994fda83653c9e743bd758925cb47a
Jameel Abu-Baker <100359408+J-AbuBaker@users.noreply.github.com>
2025-06-18 12:34:12 +0300

    Update ChangePasswordViewModel.cs

--- BookStore/BookStore/ViewModels/ChangePasswordViewModel.cs
+++ BookStore/BookStore/ViewModels/ChangePasswordViewModel.cs
@@ -1,1 +1,18 @@
+using System.ComponentModel.DataAnnotations;
 
+namespace Product_app.ViewModels
+{
+    public class ChangePasswordViewModel
+    {
+        [Required(ErrorMessage = "Current password is required.")]
+        public string CurrentPassword { get; set; }
+
+        [Required(ErrorMessage = "New password is required.")]
+        [MinLength(6, ErrorMessage = "New password must be at least 6 characters.")]
+        public string NewPassword { get; set; }
+
+        [Required(ErrorMessage = "Please confirm your new password.")]
+        [Compare("NewPassword", ErrorMessage = "Passwords do not match.")]
+        public string ConfirmPassword { get; set; }
+    }
+}
